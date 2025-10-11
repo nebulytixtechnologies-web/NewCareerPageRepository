b2038d8ed56181add26deaf699cefd5d476f8695
Admin <Admin@DESKTOP-MINO19C>
2025-10-11 15:49:23 +0530

    added setter,getter and constructor for the entity classes

--- resumeupload2/src/main/java/com/neb/dto/DeveloperRequest.java
+++ resumeupload2/src/main/java/com/neb/dto/DeveloperRequest.java
@@ -9,7 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 @Data
 @AllArgsConstructor
 public class DeveloperRequest {
-    private String role;
+	private String role;
     private String firstName;
     private String lastName;
     private String email;
@@ -23,4 +23,108 @@ public class DeveloperRequest {
     private Double duration;
     private MultipartFile resume;
     private String code;
+    public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getQualification() {
+		return qualification;
+	}
+	public void setQualification(String qualification) {
+		this.qualification = qualification;
+	}
+	public Integer getPassoutYear() {
+		return passoutYear;
+	}
+	public void setPassoutYear(Integer passoutYear) {
+		this.passoutYear = passoutYear;
+	}
+	public String getInternship() {
+		return internship;
+	}
+	public void setInternship(String internship) {
+		this.internship = internship;
+	}
+	public String getDomain() {
+		return domain;
+	}
+	public void setDomain(String domain) {
+		this.domain = domain;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public Double getSalary() {
+		return salary;
+	}
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+	public Double getDuration() {
+		return duration;
+	}
+	public void setDuration(Double duration) {
+		this.duration = duration;
+	}
+	public MultipartFile getResume() {
+		return resume;
+	}
+	public void setResume(MultipartFile resume) {
+		this.resume = resume;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public DeveloperRequest(String role, String firstName, String lastName, String email, String phone,
+			String qualification, Integer passoutYear, String internship, String domain, String companyName,
+			Double salary, Double duration, MultipartFile resume, String code) {
+		super();
+		this.role = role;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phone = phone;
+		this.qualification = qualification;
+		this.passoutYear = passoutYear;
+		this.internship = internship;
+		this.domain = domain;
+		this.companyName = companyName;
+		this.salary = salary;
+		this.duration = duration;
+		this.resume = resume;
+		this.code = code;
+	}
+	
 }
--- resumeupload2/src/main/java/com/neb/entity/DeveloperApplication.java
+++ resumeupload2/src/main/java/com/neb/entity/DeveloperApplication.java
@@ -26,4 +26,95 @@ public class DeveloperApplication {
     private double duration;
     private String resumePath;
     private Instant createdAt;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getQualification() {
+		return qualification;
+	}
+	public void setQualification(String qualification) {
+		this.qualification = qualification;
+	}
+	public int getPassoutYear() {
+		return passoutYear;
+	}
+	public void setPassoutYear(int passoutYear) {
+		this.passoutYear = passoutYear;
+	}
+	public boolean isInternship() {
+		return internship;
+	}
+	public void setInternship(boolean internship) {
+		this.internship = internship;
+	}
+	public String getDomain() {
+		return domain;
+	}
+	public void setDomain(String domain) {
+		this.domain = domain;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public double getDuration() {
+		return duration;
+	}
+	public void setDuration(double duration) {
+		this.duration = duration;
+	}
+	public String getResumePath() {
+		return resumePath;
+	}
+	public void setResumePath(String resumePath) {
+		this.resumePath = resumePath;
+	}
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Instant createdAt) {
+		this.createdAt = createdAt;
+	}
+    
 }
